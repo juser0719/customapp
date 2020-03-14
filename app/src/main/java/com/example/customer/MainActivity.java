b19a2a018bd6d31002d6f5483276f330e315e3f8
juser0719 <sinsa0719@naver.com>
2020-03-14 18:17:23 +0900

    user design

---
+++ app/src/main/java/com/example/customer/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.customer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class MainActivity extends AppCompatActivity {
+    private String WIFFLE = "miFU2Mn2ZrUcSgyhmAxLMjgDJAr2";
+    private String HODDUK = "jqG9AZCtJBfm8wCsPTgnf332dWB3";
+    private TextView wiffle_open;
+    private TextView hodduk_open;
+    private static final String TAG = "MainActivity";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    DocumentReference Ref_wiffle = db.collection("open").document(WIFFLE);
+    DocumentReference Ref_hodduk = db.collection("open").document(HODDUK);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wiffle_open = (TextView)findViewById(R.id.wiffle);
+        hodduk_open = (TextView)findViewById(R.id.hodduk);
+
+        Ref_wiffle.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Boolean open =(Boolean) document.get("open");
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                        if(open){
+                            wiffle_open.setText("오와열 :)");
+                        }
+                        else {
+                            wiffle_open.setText("오와닫 :(");
+                        }
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
+        Ref_hodduk.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Boolean open =(Boolean) document.get("open");
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                        if(open){
+                            hodduk_open.setText("홌!!");
+                        }
+                        else {
+                            hodduk_open.setText("홊ㅜ");
+                        }
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+    }
+}
