669bf8e0c24e91977c79c7af1477357a83d22691
juser0719 <sinsa0719@naver.com>
2020-03-22 16:35:57 +0900

    start costumer  appdign_ image

--- app/src/main/java/com/example/customer/MainActivity.java
+++ app/src/main/java/com/example/customer/MainActivity.java
@@ -6,11 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private String HODDUK = "jqG9AZCtJBfm8wCsPTgnf332dWB3";
     private ImageView wiffle_open;
     private ImageView hodduk_open;
+    private FloatingActionButton reset;
     private static final String TAG = "MainActivity";
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     DocumentReference Ref_wiffle = db.collection("open").document(WIFFLE);
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         wiffle_open = (ImageView)findViewById(R.id.wiffle);
         hodduk_open = (ImageView)findViewById(R.id.hodduk);
+        reset = (FloatingActionButton)findViewById(R.id.re);
 
         Ref_wiffle.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -78,6 +83,64 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     Log.d(TAG, "get failed with ", task.getException());
                 }
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"새로고침 완료!",Toast.LENGTH_SHORT).show();
+
+                Ref_hodduk.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                Boolean open =(Boolean) document.get("open");
+                                Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                                if(open){
+                                    hodduk_open.setImageResource(R.drawable.open_hodduk);
+                                    hodduk_open.setBackgroundColor(Color.rgb(246,214,127));
+                                }
+                                else {
+                                    hodduk_open.setImageResource(R.drawable.close_hodduk);
+                                    hodduk_open.setBackgroundColor(Color.rgb(212,231,241));
+                                }
+                            } else {
+                                Log.d(TAG, "No such document");
+                            }
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+                });
+
+                Ref_wiffle.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                Boolean open =(Boolean) document.get("open");
+                                Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                                if(open){
+                                    wiffle_open.setImageResource(R.drawable.open_wiffle);
+                                    wiffle_open.setBackgroundColor(Color.rgb(240,227,182));
+                                }
+                                else {
+                                    wiffle_open.setImageResource(R.drawable.close_wiffle);
+                                    wiffle_open.setBackgroundColor(Color.rgb(212,231,241));
+                                }
+                            } else {
+                                Log.d(TAG, "No such document");
+                            }
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+                });
+
             }
         });
     }
